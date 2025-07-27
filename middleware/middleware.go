bf7e5ad3cc1028abc39fa4b974839092a6f71f29
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-07-27 22:13:48 +0530

    feat : created the bauth middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,64 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/golang-jwt/jwt/v5"
+
+	"os"
+	"strings"
+)
+
+func VerifyToken (c *fiber.Ctx) error {
+	load_err := godotenv.Load()
+	if load_err != nil { 
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error" : load_err.Error(),
+		}) 
+	}
+	key := os.Getenv("JWT_SECRET")
+	authHeader := c.Get("Authorization")
+
+	if authHeader == "" { 
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error" : "Authheader not found",
+		}) 
+	}
+
+	tokenStr := strings.TrimPrefix(authHeader,"Bearer ") 
+	token,err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		return []byte(key),nil
+	})
+
+	if err != nil || !token.Valid { 
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error" : "Unauthorized",
+		})
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok { 
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error" : "invalid token claims",
+		})
+	}
+
+	token_id, ok := claims["token_id"].(string)
+	if !ok { 
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error" : "invalid token_id",
+		})
+	}
+
+	c.Locals("token_id",token_id)
+
+	userid, ok := claims["userid"].(string)
+	if !ok { 
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error" : "invalid userid",
+		})
+	}
+	c.Locals("userid",userid)
+
+	return c.Next()
+}
