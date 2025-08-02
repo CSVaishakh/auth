db033629076832ef8ca214434612c2203af19d72
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-08-02 21:51:23 +0530

    feat : added the varify token endpoint

--- main.go
+++ main.go
@@ -21,9 +21,8 @@ func main() {
 	app.Post("/signup",handlers.SignUp)
 	app.Post("/signin",handlers.SignIn)
 
-	
 	app.Post("/signout",middleware.VerifyToken,handlers.SignOut)
 	app.Get("/profile",middleware.VerifyToken,handlers.GetProfile)
-	
+	app.Get("/verify",middleware.VerifyToken)	
 	log.Fatal(app.Listen(":5000"))
 }
